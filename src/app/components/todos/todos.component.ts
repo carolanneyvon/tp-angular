766ea0047af5b21b22f4b55fe8929d855c8e4489
Carolanne Yvon <elolithia@gmail.com>
2023-03-22 10:36:44 +0100

    création du modèle todo

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/app/shared/models/todo';
+import { TodoService } from 'src/app/shared/services/todo.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
+//Avant création du service
+// export class TodosComponent {
+//   todos = [
+//     { text: 'Faire la vaisselle', done: false },
+//     { text: 'Faire le ménage', done: true },
+//     { text: 'Faire les courses', done: false },
+//   ];
+// }
 export class TodosComponent {
-  todos = [
-    { text: 'Faire la vaisselle', done: false },
-    { text: 'Faire le ménage', done: true },
-    { text: 'Faire les courses', done: false },
-  ];
+  todos: Todo[] = this._todoService.todos;
+  constructor(private _todoService: TodoService) {}
 }
---
+++ src/app/shared/services/todo.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  public todos: Todo[] = [
+    new Todo('Faire la vaisselle'),
+    new Todo('Faire le ménage'),
+    new Todo('Faire les courses'),
+    new Todo('Travailler les cours'),
+  ];
+
+  constructor() { }
+}
