986076179327f8bf125b2906e9e995b548a34e8a
Carolanne Yvon <elolithia@gmail.com>
2023-03-22 12:01:14 +0100

    Protection des données pour les todos

--- src/app/shared/services/todo.service.ts
+++ src/app/shared/services/todo.service.ts
@@ -6,16 +6,24 @@ import { Todo } from '../models/todo';
 })
 export class TodoService {
 
-  public todos: Todo[] = [
+  //public todos: Todo[] = [
+  private _todos: Todo[] = [
     new Todo('Faire la vaisselle'),
     new Todo('Faire le ménage'),
     new Todo('Faire les courses'),
     new Todo('Travailler les cours'),
   ];
 
+  // copie du tableau todo pour que seul le servcie puisse agir sur les données
+  // pour protéger les données
+  // passer le tableau en privé et faire une copie
+  // getter appeler todos donc aucune modification dans todos.components.ts et dans le footer
+  get todos(){
+    return [ ...this._todos]
+  }
   constructor() {
     setTimeout(() => {
-      this.todos.push(new Todo("Démarrer le programme"));
+      this._todos.push(new Todo("Démarrer le programme"));
     }, 2000);
   }
 }
