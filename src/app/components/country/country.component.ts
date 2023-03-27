f9b196fda9733b205e7b7d95b738ce9d8534442e
Carolanne Yvon <elolithia@gmail.com>
2023-03-27 15:33:52 +0200

    suite form

--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Country } from './../../shared/models/country';
+import { Address, User } from './../../shared/models/user';
 
 @Component({
   selector: 'app-country',
@@ -13,6 +14,13 @@ export class CountryComponent {
     new Country('dfc5de44-ed89-458b-825a-2a313e28bc9a', 'France'),
     new Country('b0be11c0-bdd0-4621-ae30-5645b77e480a', 'Espagne'),
   ];
+
+  public contact = new User();
+  public address = new Address();
+
+  constructor() {
+    this.contact.address = this.address;
+  }
 
   onSubmit(form: NgForm) {
     console.log(form)
--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -5,9 +5,18 @@ export class User {
     public id?: string,
     public lastname?: string,
     public firstname?: string,
+    public address?: Address,
     public email?: string,
     public gender?: string,
     public isMarried?: boolean,
     public country?: Country,
+  ) {}
+}
+
+export class Address {
+  constructor(
+    public street?: string,
+    public zipcode?: string,
+    public city?: string,
   ) {}
 }
