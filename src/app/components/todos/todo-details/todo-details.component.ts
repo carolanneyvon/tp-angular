3d1386c093e539a76e6dbe2e28c6bd57e11451bd
Carolanne Yvon <elolithia@gmail.com>
2023-03-30 09:43:40 +0200

    Refacto + delete + détail

--- src/app/components/todos/todo-details/todo-details.component.ts
+++ src/app/components/todos/todo-details/todo-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from 'src/app/shared/models/todo';
+import { TodoService } from 'src/app/shared/services/todo.service';
 
 @Component({
   selector: 'app-todo-details',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-details.component.css']
 })
 export class TodoDetailsComponent {
+  @Input() todo!: Todo|null; // recoit du parent, la liste qui renvoie la tache
+  
+  constructor(private _todoService: TodoService) {}
 
+  deleteTodo(id?: string) { //pour supprimer la tache de la vue détails
+    this._todoService.delete(id);
+    this.todo = null;
+  }
 }
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -10,13 +10,28 @@ import { TodoService } from 'src/app/shared/services/todo.service';
 
 export class TodosComponent {
   todos$ = this._todoService.todos$;
+  selected!: Todo;
   
   constructor(
     private _todoService: TodoService
   ) {}
 
+  selectTodo(selected: Todo) {
+    this.selected = selected; // peu également être fait directement dans le html
+  }
+
   toggleStateTodo(todo: Todo) {
     todo.done = !todo.done;
     this._todoService.update(todo);
+  }
+
+  editTodo(todo: Todo){ // recupère une chaine de caractère ou undefined
+    console.log("edition de la todo...");
+  }
+
+  deleteTodo(id?: string){ // recupère une chaine de caractère ou undefined
+    //if (id){ // si id existe alors suppression => deplacer dans le service
+      this._todoService.delete(id);
+    //}
   }
 }
